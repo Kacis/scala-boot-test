45e6df7b4e59d9a8aac8c26eb172b3d7809fa81e
Ilkka Anttonen <ilkka.anttonen@gmail.com>
2015-04-20 20:32:22 +0300

    Added language indicator to the returned message,

--- src/main/scala/hello/Message.scala
+++ src/main/scala/hello/Message.scala
@@ -4,4 +4,4 @@ import java.util.Date
 
 import scala.beans.BeanProperty
 
-case class Message(@BeanProperty hostname: String, @BeanProperty time: Date)
+case class Message(@BeanProperty hostname: String, @BeanProperty time: Date, @BeanProperty language: String = "Scala")
